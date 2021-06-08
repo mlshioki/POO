7d01b79bf89eb1848a5fc48f4f006f8055728716
Sayuri Hioki <sayuri.hioki12@gmail.com>
2021-06-08 19:01:26 -0300

    att 2

--- trabalho-semestral/app/Http/Controllers/ClientesController.php
+++ trabalho-semestral/app/Http/Controllers/ClientesController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
+    public function index()
+    {
+        return view('cliente.cliente')->with('clientes', Clientes::All());
+    }
+
     public function listar(){
 
         $clientes = Clientes::all();
-        return view('clientes.listar', ['clientes'-> $clientes]);
+        return view('clientes.listar', ['clientes' => $clientes]);
     }
 }
